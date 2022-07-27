01c1a011e304f2c6aaf1b7e067533355d660970f
Or Cohen <or672001@gmail.com>
2022-07-27 11:17:39 +0300

    Revmoed unused imports

--- src/main/java/oros/sereneseasonfix/Sereneseasonfix.java
+++ src/main/java/oros/sereneseasonfix/Sereneseasonfix.java
@@ -1,22 +1,8 @@
 package oros.sereneseasonfix;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("sereneseasonfix")
@@ -25,6 +11,7 @@ public class Sereneseasonfix {
     // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger("sereneseasonfix");
 
+    // Idk if I can just remove this class altogether, so imma just keep the bare minimum.
     public Sereneseasonfix() {
     }
 }
